552f8726024d56338537a5e47ea7d982ef7c7f0a
Brendan McCollam <bmccollam@leapfrogonline.com>
2012-01-16 10:53:28 -0600

    Improving phone_number generator to conform to NANP

--- src/faker/generators/utils.py
+++ src/faker/generators/utils.py
@@ -2,11 +2,17 @@ import random
 import re
 import string
 
-numerify_re = re.compile('#')
+numerify_re = re.compile('#|\+')
 letterify_re = re.compile(r'\?')
 
+def _numerify_helper(match_obj):
+    if match_obj.group() == '+':
+        return str(random.randint(2, 9))
+    else:
+        return str(random.randint(0, 9))
+
 def numerify(number_string):
-    return numerify_re.sub(lambda m: str(random.randint(0, 9)), number_string)
+    return numerify_re.sub(_numerify_helper, number_string)
 
 def letterify(letter_string):
     return letterify_re.sub(lambda m: random.choice(string.ascii_letters), letter_string)
